25eafe9fbc4aa3410b4bb6dc4abac44423ad114f
Jonathan Girard Viau <girardvjonathan@gmail.com>
2016-06-04 16:03:21 -0400

    mostly refactor

--- main.js
+++ main.js
@@ -34,8 +34,5 @@ var settings = {
 var bot = new Bot(settings);
 
 bot.on('start', function() {
-    bot.postMessageToChannel('general', 'Hello channel!');
-    bot.postMessageToUser('kimsly', 'hello bro!');
-    bot.postMessageToUser('girardvjonathan', 'hello bro!');
     //bot.postMessageToGroup('some-private-group', 'hello group chat!');
 });
