832b0e44c32848254691bc084924fc545f2c1127
Kim L <contact@kimsangly.com>
2016-06-04 16:04:52 -0400

    Add image post to Slack with slackbots.

--- getphoto.js
+++ getphoto.js
@@ -6,7 +6,7 @@ exports.search = function (searchTerm, searchResult) {
     var api500px = new API500px(k);
 
     return api500px.photos.searchByTerm(searchTerm, {'sort': 'created_at', 'rpp': '100'}, function(error, results) {
-        var photoUrl = "No results found for \"" + searchTerm + "\"";
+        var photoResponse = {};
 
         if (error) {
             console.log("Error! " + error);
@@ -16,11 +16,15 @@ exports.search = function (searchTerm, searchResult) {
             if(results.photos) {
                 var random = Math.floor((Math.random() * (results.photos.length-1)) + 0);
                 var photo = results.photos[random];
-                var randomPhotoUrl = 'https://500px.com' + photo.url;
-                photoUrl = randomPhotoUrl;
+                var randomPhotoPage = 'https://500px.com' + photo.url;
+                var randomPhotoUrl = photo.image_url;
+                photoResponse = {
+                    pageUrl: randomPhotoPage,
+                    imageUrl: randomPhotoUrl
+                };
             }
         }
 
-        searchResult(photoUrl);
+        searchResult(photoResponse);
     });
 };
